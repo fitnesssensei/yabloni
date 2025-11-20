e02fd92bd3b49dd8c606cfdbbfe0143d40a1bf23
rustam <dsvs>
2025-11-20 18:15:23 +0300

    садовод

--- config/urls.py
+++ config/urls.py
@@ -20,11 +20,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
+from catalog.views import HomeView
+
 
 urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('', include('catalog.urls', namespace='catalog')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
