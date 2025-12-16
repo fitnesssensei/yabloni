bc9465ab95e5cce4a806f1d9305395d21be4071d
rustam <dsvs>
2025-12-16 13:17:50 +0300

    работает вся шапка

--- config/urls.py
+++ config/urls.py
@@ -21,22 +21,34 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
-from catalog.views import HomeView
+from django.contrib.auth import views as auth_views
+from catalog.views import HomeView, register
 
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
+    
+    # Аутентификация
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    #path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(
+        template_name='registration/logged_out.html',
+        next_page='home'
+    ), name='logout'),
+    path('register/', register, name='register'),
+    
+    # Приложения
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('', include('catalog.urls', namespace='catalog')),
+    path('catalog/', include('catalog.urls', namespace='catalog')),
     
-    # Добавляем URL для поиска, Питомник, Блог, аккаунт
+    # Статические страницы
     path('nursery/', TemplateView.as_view(template_name='nursery.html'), name='nursery'),
     path('blog/', TemplateView.as_view(template_name='blog.html'), name='blog'),
     path('search/', TemplateView.as_view(template_name='search.html'), name='search'),
     path('account/', TemplateView.as_view(template_name='account.html'), name='account'),
-
-
+    
+    # Главная страница
+    path('', HomeView.as_view(), name='home'),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
