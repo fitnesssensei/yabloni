8e31d63285557e5ce2aef3f4218a333b712f21d3
rustam <dsvs>
2025-12-26 16:15:47 +0300

    отступы меньше

--- config/urls.py
+++ config/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 ###### главный URL файл ######
 
 from django.contrib import admin
@@ -24,6 +8,7 @@ from django.views.generic import TemplateView
 from django.contrib.auth import views as auth_views
 from catalog.views import HomeView, register
 from django.views.generic import TemplateView
+from . import views
 
 
 urlpatterns = [
@@ -46,7 +31,8 @@ urlpatterns = [
     
     # Статические страницы
     path('nursery/', TemplateView.as_view(template_name='header/nursery.html'), name='nursery'),
-    path('blog/', TemplateView.as_view(template_name='header/blog.html'), name='blog'),
+    path('blog/', include('blog.urls', namespace='blog')),
+    #path('blog/', TemplateView.as_view(template_name='header/blog.html'), name='blog'),
     path('search/', TemplateView.as_view(template_name='header/search.html'), name='search'),
     path('account/', TemplateView.as_view(template_name='header/account.html'), name='account'),
     
