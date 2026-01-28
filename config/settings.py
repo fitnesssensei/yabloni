b40670c3a1f9ff2d35e0c613a2c68cf1773b98bc
rustam <dsvs>
2026-01-28 15:55:42 +0300

    4 кнопки в каталоге

--- config/settings.py
+++ config/settings.py
@@ -161,15 +161,12 @@ ORDER_STATUS_CHOICES = (
 # Настройки электронной почты сайта
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # консольный вариант 
-EMAIL_HOST = 'smtp.gmail.com'  # или другой SMTP-сервер
-EMAIL_PORT = 587
-# было : 
-EMAIL_USE_TLS = True
-# стало : 
-#EMAIL_USE_SSL = True
-EMAIL_HOST_USER = '13fitnesssensei@gmail.com'  # замените на ваш email
-EMAIL_HOST_PASSWORD = 'mcapdjeeljfbskmj'  # пароль приложения, не от аккаунта!
-DEFAULT_FROM_EMAIL = '13fitnesssensei@gmail.com'  # тот же email, что и EMAIL_HOST_USER
+EMAIL_HOST = 'smtp.yandex.ru'  # Yandex SMTP-сервер
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  # email из .env
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # пароль приложения из .env
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')  # email из .env
 
 # Логирование
 LOGGING = {
