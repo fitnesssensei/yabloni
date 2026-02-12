d6d8e58263a546db11a75b59b1dd2859a9544e0c
rustam <dsvs>
2026-02-12 17:20:54 +0300

    идентификация отзывов

--- reviews/views.py
+++ reviews/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import ReviewForm
+from orders.models import Order
 
 def add_review(request):
     if request.method == 'POST':
         form = ReviewForm(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request, 'Спасибо за ваш отзыв! Он будет опубликован после модерации.')
-            return redirect('nursery')
+            email = form.cleaned_data['email']
+            if Order.objects.filter(email=email).exists():
+                form.save()
+                messages.success(request, 'Спасибо за ваш отзыв! Он будет опубликован после модерации.')
+                return redirect('nursery')
+            else:
+                messages.error(request, 'Отзыв могут оставить только те, кто уже сделал заказ. Проверьте email.')
+                return render(request, 'reviews/add_review.html', {'form': form})
     else:
         form = ReviewForm()
     return render(request, 'reviews/add_review.html', {'form': form})
