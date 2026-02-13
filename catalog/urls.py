3c27bc17bef4ef7098fecf82d744b121ac4899f8
rustam <dsvs>
2026-02-13 14:54:39 +0300

    остатки только в подробн странице

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('search/', views.product_search, name='product_search'),  # Новый маршрут для поиска
     path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
-    path('<slug:category_slug>/<slug:subcategory_slug>/', views.product_list, name='product_list_by_category_subcategory'),
     path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('<slug:category_slug>/<slug:subcategory_slug>/', views.product_list, name='product_list_by_category_subcategory'),
 ]
