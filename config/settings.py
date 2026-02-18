f5256b59e3d31bb4c62a35028ba229bd6e52d8f4
rustam <dsvs>
2026-02-18 16:31:22 +0300

    подвал сайта

--- config/settings.py
+++ config/settings.py
@@ -141,7 +141,7 @@ LANGUAGE_CODE = 'ru-ru'
 TIME_ZONE = 'Europe/Moscow'
 
 # настройки для статических файлов
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
